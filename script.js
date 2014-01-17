4f9f45c90218e85ba1aad3ed92c06b49cc07a149
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-16 23:43:34 -0800

    hexes exist. they repeat for columns and rows. next step get them offset

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 // make ourselves a canvas in the empty index
 var svg = d3.select('body').append('svg')
-var height = y = 500;
-var width = x = 500;
-svg.attr({'width': x, 'height': y});
+var svgx =  1600;
+var svgy =  1000;
+svg.attr({'width': svgx, 'height': svgy});
 
+// constants
+hexsize = 50
+ 
+// find the centers of all our hexagons, we'll return a 'data' element
+// then we'll pass the 'data' to a d3.selectAll to create hexagons
+var hexCenters = function(screenheight, screenwidth, size) {
+  var hexheight = size * 2
+  var hexwidth = Math.sqrt(3)/2 * size
+  var vertical = 3/4 * hexheight
+  var horizontal = 2 * size;
 
-//define points for a <polygon> element
-var hexPoints = function(hxlocation, size) {
-  var el = '<polygon></polygon>'
+  var data = [];
+
+  var rows = screenheight / vertical
+  var columns = screenwidth / horizontal
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+      data.push({x: horizontal*i, y: vertical*j })
+    };
+  };
+  
+  return data;
+}
+
+//define points for a <polygon> element that is a hexagon
+var hexPoints = function(hx, hy, size) {
   var points = ''
   for (i = 0; i < 6; i++){
     angle = 2 * Math.PI / 6 * ( i +0.5)
-    x_i = hxlocation.x + size * Math.cos(angle);
-    y_i = hxlocation.y + size * Math.sin(angle);
+    x_i = hx + size * Math.cos(angle);
+    y_i = hy + size * Math.sin(angle);
     points += x_i + ',' + y_i + ' '
   }
   return points
 }
 
-svg.append('polygon').attr('points', hexPoints({x:100,y:100},100))
+// append a single polygon element to the svg :) 
+svg.append('polygon').attr('points', hexPoints(100, 100 ,hexsize))
 
+// so how do we append a few polygons? 
+
+var hexcount = hexCenters(svgy, svgx, hexsize)
+
+svg.selectAll('polygon').data(hexcount)
+  .enter()
+  .append('polygon')
+  .attr('points', function(d){ return hexPoints(d.x, d.y, hexsize)})
