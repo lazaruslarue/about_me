9bd7d74328c2e1d037eb3b0f17fa6b8964a7b2dd
Mr. Spothawk <mr.spothawk@gmail.com>
2014-01-19 22:26:55 -0800

    works

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
 // make ourselves a canvas in the empty index
-var svg = d3.select('body').append('svg')
+var svg = d3.select('body').append('svg');
 var svgx =  1600;
 var svgy =  1000;
 svg.attr({'width': svgx, 'height': svgy});
 
 // constants
-hexsize = 33
- 
+hexsize = 14;
+
 // find the centers of all our hexagons, we'll return a 'data' element
 // then we'll pass the 'data' to a d3.selectAll to create hexagons
 var hexCenters = function(screenheight, screenwidth, size) {
-  var hexheight = size * 2
-  var hexwidth = Math.sqrt(3)/2 * size
-  var vertical = 3/4 * hexheight
+  var hexheight = size * 2;
+  var hexwidth = Math.sqrt(3)/2 * size;
+  var vertical = 3/4 * hexheight;
   var horizontal = hexwidth;
 
   var data = [];
 
-  var rows = screenheight / vertical
-  var columns = screenwidth / horizontal
+  var rows = screenheight / vertical;
+  var columns = screenwidth / horizontal;
 
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
-      data.push({x: (hexwidth)*(i%2) + 2*hexwidth*j , y: vertical*i })
-    };
-  };
+      data.push({x: (hexwidth)*(i%2) + 2*hexwidth*j , y: vertical*i });
+    }
+  }
   
   return data;
-}
+};
 
 //define points for a <polygon> element that is a hexagon
 var hexPoints = function(hx, hy, size) {
-  var points = ''
+  var points = '';
   for (i = 0; i < 6; i++){
-    angle = 2 * Math.PI / 6 * ( i +0.5)
+    angle = 2 * Math.PI / 6 * ( i +0.5);
     x_i = hx + size * Math.cos(angle);
     y_i = hy + size * Math.sin(angle);
-    points += x_i + ',' + y_i + ' '
+    points += x_i + ',' + y_i + ' ';
   }
-  return points
-}
+  return points;
+};
 
 // append a single polygon element to the svg :) 
-svg.append('polygon').attr('points', hexPoints(100, 100 ,hexsize))
+svg.append('polygon').attr('points', hexPoints(100, 100 ,hexsize));
 
 // so how do we append a few polygons? 
 
-var hexcount = hexCenters(svgy, svgx, hexsize)
+var hexcount = hexCenters(svgy, svgx, hexsize);
 
-svg.selectAll('polygon').data(hexcount)
+svg.selectAll('g').data(hexcount)
   .enter()
   .append('polygon')
-  .attr('points', function(d){ return hexPoints(d.x, d.y, hexsize)})
-  
-
-
-
-
+  .attr('points', function(d){ 
+    return hexPoints(d.x, d.y, hexsize);
+  }); 
